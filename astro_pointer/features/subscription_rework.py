c9787fdd4f93de03db086d1bdbdd49311f85ec0e
Kaito <tommychoiht@gmail.com>
2022-07-30 23:06:24 +0800

    Planning to rework the subscription system

---
+++ astro_pointer/features/subscription_rework.py
@@ -0,0 +1,16 @@
+"""
+Ideas:
+
+use ConversationHandler
+
+Steps:
+1. Ask for which features to subscribe to
+2. Daily / Weekly / Monthly / Yearly ?
+3. Ask for the date / time
+4. Go back to step 2 for another subscription
+
+Date/time picker
+
+What if the user stops halfway through the process?
+
+"""
